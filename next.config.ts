11c239edd310f665937753b929e5079948fdaee4
anthonyb8 <anthonybaxter819@gmail.com>
2025-06-11 14:22:10 -0400

    Initial commit.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "dist", // output director
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
